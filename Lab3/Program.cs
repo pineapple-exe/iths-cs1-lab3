1fa03dcf9783deb07b26b0089379fb9a31a1c1ad
Johanna Forssén <forssen.johanna@gmail.com>
2020-10-01 11:23:48 +0200

    Clean up code and print full chunk length

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 
 namespace Lab3
 {
@@ -27,32 +28,34 @@ namespace Lab3
         static void PrintImageMetadata(string path)
         {
             FileStream dataStream = new FileStream(path, FileMode.Open);
-            BinaryReader interpreter = new BinaryReader(dataStream);
-            short maybeBM = interpreter.ReadInt16();
+            BinaryReader reader = new BinaryReader(dataStream);
+            short maybeBM = reader.ReadInt16();
 
             if (maybeBM == 19778)
             {
                 dataStream.Position = 18;
-                int width = interpreter.ReadInt32();
-                int height = interpreter.ReadInt32();
+                int width = reader.ReadInt32();
+                int height = reader.ReadInt32();
                 Console.WriteLine($"This is a .bmp image. Resolution: {width}x{height} pixels.");
                 return;
             }
 
             dataStream.Position = 0;
-            long maybePNG = interpreter.ReadInt64();
+            long maybePNG = reader.ReadInt64();
 
             if (maybePNG == 727905341920923785)
             {
-                dataStream.Position = 16; //PNG image, consisting of a series of chunks beginning with an IHDR chunk.
-                int width = interpreter.ReadInt32BigEndian();
-                int height = interpreter.ReadInt32BigEndian();
+                dataStream.Position = 16;
+                int width = reader.ReadInt32BigEndian();
+                int height = reader.ReadInt32BigEndian();
                 Console.WriteLine($"This is a .png image. Resolution: {width}x{height} pixels.");
+                Console.WriteLine();
 
                 dataStream.Position = 8;
+                Console.WriteLine("---- Chunk list ----");
                 while (dataStream.Position < dataStream.Length)
                 {
-                    PrintTypeNLengthPNG(dataStream, interpreter);
+                    PrintTypeNLengthPNG(dataStream, reader);
                 }
 
                 return;
@@ -61,19 +64,19 @@ namespace Lab3
             Console.WriteLine("This is not a valid .bmp or .png file!");
         }
 
-        static void PrintTypeNLengthPNG(FileStream dataStream, BinaryReader interpreter)
+        static void PrintTypeNLengthPNG(FileStream dataStream, BinaryReader reader)
         {
-            int chunkDataLength = interpreter.ReadInt32BigEndian();
-            string chunkType = System.Text.Encoding.ASCII.GetString(interpreter.ReadBytes(4));
+            int chunkDataLength = reader.ReadInt32BigEndian();
+            string chunkType = Encoding.ASCII.GetString(reader.ReadBytes(4));
 
-            Console.WriteLine($"Type: {chunkType}    Length of chunk data: {chunkDataLength}");
+            Console.WriteLine($"Type: {chunkType}      Length of chunk: {chunkDataLength + 12} bytes");
 
             dataStream.Position += chunkDataLength + 4;
         }
 
-        static int ReadInt32BigEndian(this BinaryReader interpreter)
+        static int ReadInt32BigEndian(this BinaryReader reader)
         {
-            byte[] bytes = interpreter.ReadBytes(4);
+            byte[] bytes = reader.ReadBytes(4);
             byte[] reversed = new byte[bytes.Length];
             int rIndex = 0;
 
